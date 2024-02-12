b615a951ca3103885b8d5400b3b5aed6de93192c
Veronica Garcia <veroagarcia90@gmail.com>
2024-02-12 19:31:27 -0300

    estaba mal el nombre en la base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://veronicaagarcia.gitlab.io/TheDoGame/'
+  base:'https://veronicaagarcia.github.io/TheDoGame/'
 })
